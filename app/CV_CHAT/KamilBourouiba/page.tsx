16705523ffdd0ab6da68a63096a6ec007b12d659
Kamil <kbourouiba@icloud.com>
2024-11-08 15:17:48 +0100

    fix mobile

--- app/CV_CHAT/KamilBourouiba/page.tsx
+++ app/CV_CHAT/KamilBourouiba/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Head from "next/head"; // Import Head from next/head
 import styles from "../shared/page.module.css";
 
 import Chat from "../../components/chat";
@@ -7,18 +8,24 @@ import FileViewer from "../../components/file-viewer";
 
 const KamilPage = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.column}>
-          <FileViewer />
-        </div>
-        <div className={styles.chatContainer}>
-          <div className={styles.chat}>
-            <Chat />
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Kamil Page</title> {/* Optional: Set a title for your page */}
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.column}>
+            <FileViewer />
+          </div>
+          <div className={styles.chatContainer}>
+            <div className={styles.chat}>
+              <Chat />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
