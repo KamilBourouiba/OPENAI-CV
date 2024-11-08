b4bd0c1548aa1fa10f68495b042f23c293dfee3a
Kamil <kbourouiba@icloud.com>
2024-11-08 15:29:57 +0100

    fix mobile

--- app/CV_CHAT/KamilBourouiba/page.tsx
+++ app/CV_CHAT/KamilBourouiba/page.tsx
@@ -1,46 +1,31 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
+import Head from "next/head"; // Import Head from next/head
 import styles from "../shared/page.module.css";
 
 import Chat from "../../components/chat";
 import FileViewer from "../../components/file-viewer";
 
 const KamilPage = () => {
-  const inputRef = useRef(null); // Create a ref for the input field
-
-  const handleSend = () => {
-    // Add your send logic here
-    console.log("Send button clicked");
-
-    // Blur the input field to prevent zoom
-    if (inputRef.current) {
-      inputRef.current.blur();
-    }
-  };
-
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.column}>
-          <FileViewer />
-        </div>
-        <div className={styles.chatContainer}>
-          <div className={styles.chat}>
-            <Chat />
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Kamil Page</title> {/* Optional: Set a title for your page */}
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.column}>
+            <FileViewer />
           </div>
-          <div>
-            <input
-              ref={inputRef} // Attach the ref to the input
-              type="text"
-              placeholder="Enter your question"
-            />
-            <button className={styles.sendButton} onClick={handleSend}>
-              Send
-            </button>
+          <div className={styles.chatContainer}>
+            <div className={styles.chat}>
+              <Chat />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
